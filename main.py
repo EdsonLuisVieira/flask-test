97bc4deb786796899e4c65a1fdc900dcef5620ea
EdsonLuisVieira <edson.l.v.almeida@outlook.com>
2020-08-25 15:04:40 -0300

    teste sonar credentials

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    secret='baoaboaaoaaoab'
+    credentials='usususususus'
+    user="blblblblb"
+    key='iiwiwiwiwiwi'
     return 'Hello, World!'
